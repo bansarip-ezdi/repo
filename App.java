59a8163d225c4a60505a4f0870670f79396cc7f5
bansarip-ezdi <35217341+bansarip-ezdi@users.noreply.github.com>
2018-01-08 19:15:23 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,20 @@
+package respo.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+   
+    {
+    	int a=1;
+      if(a==1)
+    
+    	 System.out.println( "Hello World!" );
+      else
+    	  System.out.println("error");
+    }
+    
+}
